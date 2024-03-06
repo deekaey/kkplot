98646ae11ff97c94f36d6987376a4896cdae051d
David Kraus <david.kraus@kit.edu>
2024-03-06 12:52:18 +0100

    resolve issue of column creation

--- src/kkplot/providers/group.py
+++ src/kkplot/providers/group.py
@@ -11,9 +11,9 @@ if __name__ == '__main__' :
 
     df = pd.read_csv( sys.argv[1], comment="#", sep="\t")
     if 'datetime' in df:
-        df.datetime = pd.to_datetime(df.datetime)
+        df['datetime'] = pd.to_datetime(df.datetime)
     elif 'date' in df:
-        df.datetime = pd.to_datetime(df.date)
+        df['datetime'] = pd.to_datetime(df.date)
         df.drop(columns=['date'], inplace=True)
 
     for a in sys.argv:
