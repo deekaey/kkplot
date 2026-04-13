303bbe2a562ce83f3a237e9ac85126674ded55d5
Kraus <kraus-d@GAP-L282A1.local>
2026-04-13 20:47:07 +0200

    update for python3 and proper setup

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
-
 import glob
 import os
-
 from setuptools import find_packages, setup
 
 DISTNAME = "kkplot"
@@ -16,7 +14,7 @@ PYTHON_REQUIRES = ">=3.7"
 
 CLASSIFIERS = [
     "Development Status :: 3 - Alpha",
-    "License :: OSI Approved :: MIT",
+    "License :: OSI Approved :: MIT License",
     "Operating System :: OS Independent",
     "Intended Audience :: Science/Research",
     "Programming Language :: Python",
@@ -30,16 +28,22 @@ CLASSIFIERS = [
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-# Get the long description from the README file
 with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
     LONG_DESCRIPTION = f.read()
 
-# get the dependencies and installs
-with open(os.path.join(here, "requirements.txt"), encoding="utf-8") as f:
-    all_reqs = f.read().split("\n")
-
-INSTALL_REQUIRES = [x.strip() for x in all_reqs if "git+" not in x]
-DEPENDENCY_LINKS = [x.strip().replace("git+", "") for x in all_reqs if "git+" not in x]
+INSTALL_REQUIRES = [
+    "bokeh",
+    "holoviews",
+    "pylatexenc",
+    "matplotlib",
+    "numexpr>=2.7.0",
+    "numpy",
+    "pandas",
+    "pillow",
+    "pyyaml",
+    "scipy",
+    "python-dotenv",
+]
 
 setup(
     name=DISTNAME,
@@ -50,7 +54,7 @@ setup(
     url=URL,
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
-    long_description_content_type="text/markdown",  # 🆕 ensures PyPI renders README.md
+    long_description_content_type="text/markdown",
     classifiers=CLASSIFIERS,
     python_requires=PYTHON_REQUIRES,
     packages=find_packages("src", exclude=["docs", "tests", "tests.*"]),
@@ -60,15 +64,14 @@ setup(
         for p in glob.glob(os.path.join("src", "*.py"))
     ],
     install_requires=INSTALL_REQUIRES,
-    dependency_links=DEPENDENCY_LINKS,
-    include_package_data=True,  # ensures MANIFEST.in is respected
-    zip_safe=False,             # safer for packages that load data files at runtime
+    include_package_data=True,
+    zip_safe=False,
     entry_points={
         "console_scripts": [
             "kkplot=kkplot.kkplot:main",
         ],
     },
-    project_urls={  # 🆕 Optional, improves PyPI page
+    project_urls={
         "Source": URL,
         "Tracker": f"{URL}/issues",
     },
--- src/kkplot/kkgen.py
+++ src/kkplot/kkgen.py
@@ -1,257 +1,318 @@
-#!/usr/bin/python
-
+#!/usr/bin/env python3
 import sys
 import argparse
 import os
+
 import kkplot.kkutils as utils
 from kkplot.kkutils.log import *
 from kkplot.kkplot_version import __version__
 
 
-class kkgen_configuration( object) :
-
-    def  __init__( self) :
-
+class KKGenConfiguration:
+    def __init__(self):
         parser = argparse.ArgumentParser()
-
-        parser.add_argument( '--names', default=None,
-            help='Legend names for each data file (E.g., --names="Scenario Dry:Scenario Wet"')
-## ??        parser.add_argument( '--names-pattern', default=None,
-## ??            help='Legend names formed from data filename (E.g., --names-pattern=""')
-
-        parser.add_argument( '--columns', default=':',
-            help='Use specified columns (i.e., named headers) from data files. Either all columns (:), all starting at C (C:) or all from Cb to Ce (Cb:Ce), or single columns delimited by comma (E.g., --columns="dN_no_emis:dN_nh3_emis,dN_n2_emis")')
-        parser.add_argument( '--columns-delim', default='\t',
-            help='Data file columns delimiter (default=<TAB>)')
-        parser.add_argument( '--show-columns', action='store_true',default=False,
-            help='Dump canonicalized column names to stdout')
-
-        parser.add_argument( '--outputformat', default='yaml',
-            help='File format for resulting script')
-        parser.add_argument( '--diff', action='store_true',default=False,
-            help='Generate difference script for data files')
-
-        parser.add_argument( '--title', default='null',
-            help='Plot title (default none)')
-        parser.add_argument( '--range', default='RANGE',
-            help='Range to plot. E.g., time period (default RANGE)')
-
-        parser.add_argument( '--debug', action='store_true', default=False,
-            help='Switch on debug mode')
-
-
-        parser.add_argument( '-V', '--version', action='store_true', default=False,
-            help='show version')
-
-
-        parser.add_argument( 'datafiles', nargs='*', default='-',
-            help='Data files (default="-" (stdin))')
+        parser.add_argument(
+            "--names",
+            default=None,
+            help=(
+                'Legend names for each data file '
+                '(e.g., --names="Scenario Dry:Scenario Wet")'
+            ),
+        )
+        parser.add_argument(
+            "--columns",
+            default=":",
+            help=(
+                "Use specified columns (named headers) from data files. Either all "
+                "columns (:), all starting at C (C:) or all from Cb to Ce (Cb:Ce), or "
+                "single columns delimited by comma "
+                '(e.g., --columns="dN_no_emis:dN_nh3_emis,dN_n2_emis")'
+            ),
+        )
+        parser.add_argument(
+            "--columns-delim",
+            default="\t",
+            help="Data file columns delimiter (default=<TAB>)",
+        )
+        parser.add_argument(
+            "--show-columns",
+            action="store_true",
+            default=False,
+            help="Dump canonicalized column names to stdout",
+        )
+        parser.add_argument(
+            "--outputformat",
+            default="yaml",
+            help="File format for resulting script",
+        )
+        parser.add_argument(
+            "--diff",
+            action="store_true",
+            default=False,
+            help="Generate difference script for data files",
+        )
+        parser.add_argument(
+            "--title",
+            default="null",
+            help="Plot title (default none)",
+        )
+        parser.add_argument(
+            "--range",
+            dest="range_",
+            default="RANGE",
+            help="Range to plot, e.g. time period (default RANGE)",
+        )
+        parser.add_argument(
+            "--debug",
+            action="store_true",
+            default=False,
+            help="Switch on debug mode",
+        )
+        parser.add_argument(
+            "-V",
+            "--version",
+            action="store_true",
+            default=False,
+            help="Show version",
+        )
+        parser.add_argument(
+            "datafiles",
+            nargs="*",
+            default=["-"],
+            help='Data files (default="-" (stdin))',
+        )
 
         self.args = parser.parse_args()
 
-        kklog.set_debug( self.args.debug)
-        kklog.set_color( self.args.debug)
+        kklog.set_debug(self.args.debug)
+        kklog.set_color(self.args.debug)
 
-        self.opts = dict()
-
-    @property
-    def  showversion( self) :
-        return  self.args.version
+        # additional figure options that can be passed from main()
+        self.opts = {}
 
     @property
-    def  datafiles( self) :
-        return  self.args.datafiles
+    def showversion(self):
+        return self.args.version
 
     @property
-    def  names( self) :
-        names = [] if self.args.names is None else self.args.names.split( ':')
+    def datafiles(self):
+        return self.args.datafiles
+
+    @property
+    def names(self):
+        names = [] if self.args.names is None else self.args.names.split(":")
         d = len(self.datafiles)
         n = len(names)
-        return names + [ '%d'%(r+n) for r in range(max(0, d-n)) ]
+        # fill up missing names with numeric labels
+        return names + [f"{r + n}" for r in range(max(0, d - n))]
 
     @property
-    def  columns( self) :
-        return  self.args.columns
-    @property
-    def  columnsdelim( self) :
-        return  self.args.columns_delim
-    @property
-    def  showcolumns( self) :
-        return  self.args.show_columns
+    def columns(self):
+        return self.args.columns
 
     @property
-    def  diff( self) :
-        return  self.args.diff
+    def columnsdelim(self):
+        return self.args.columns_delim
 
     @property
-    def  outputformat( self) :
-        return  self.args.outputformat
+    def showcolumns(self):
+        return self.args.show_columns
 
     @property
-    def  engine( self) :
-        return 'matplotlib'
+    def diff(self):
+        return self.args.diff
+
     @property
-    def  title( self) :
-        if  self.args.title=='null' :
-            return 'null'
-        return '"%s"' % ( self.args.title)
+    def outputformat(self):
+        return self.args.outputformat
+
     @property
-    def  range( self) :
-        return  self.args.range
+    def engine(self):
+        return "matplotlib"
 
-def canonicalizename( _name) :
-    validchars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789'
-    name = _name.strip( ' \t./\\')
-    canonicalized_name = ''
-    for c in name :
-        if not c in validchars :
-            canonicalized_name += '_'
-        else :
-            canonicalized_name += c
-    return canonicalized_name
+    @property
+    def title(self):
+        if self.args.title == "null":
+            return "null"
+        return f'"{self.args.title}"'
 
-def datasourceid( _filename, _slot) :
-    f = '%s_%05d' % ( _filename[_filename.rfind(os.sep)+1:].strip(), _slot)
-    return canonicalizename( f)
+    @property
+    def range(self):
+        return self.args.range_
 
 
-def readheader( _filename, _delim) :
-    F = open( _filename, 'r')
-    H = F.readline()
-    F.close()
-    H = H.split( _delim)
-    unit_offs = lambda L, pos : L if pos == -1 else pos
-    return [ c[:unit_offs( len(c), c.find( '['))] for c in H ]
-
-def findplotheaders( _header, _columns) :
-    if _columns[0] == '+' :
-        return _header[_header.index(_columns[1:])+1:]
-    C = list()
-    columns = _columns.split( ',')
-    ## example: columns="c1,c2,c3:c4,c5:"
-    for column_range in columns :
-        if ':' in column_range :
-            c1, c2 = column_range.split( ':')
-            if c1 == '' and c2 == '' :
-                C += _header[:]
-            if c2 == '' :
-                C += _header[_header.index( c1):]
-            else :
-                C += _header[_header.index( c1):_header.index( c2)+1]
-        else :
-            C.append( _header[_header.index( column_range)])
-    kklog_debug( 'plot-headers='+str(C))
-    return C
+def canonicalizename(name_raw: str) -> str:
+    validchars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789"
+    name = name_raw.strip(" \t./\\")
+    return "".join(c if c in validchars else "_" for c in name)
 
 
-def kkscript_preamble( _config) :
-    return 'engine: \"%s\"' % ( _config.engine)
-
-def kkscript_datasources( _config) :
-    D = list()
-    for l, f in enumerate( _config.datafiles) :
-        fid = datasourceid( f, l)
-        D.append( '  %s:\n    path: \"%s\"' % ( fid, f))
-    return 'datasources:\n%s' % ( '\n'.join( D))
+def datasourceid(filename: str, slot: int) -> str:
+    base = filename[filename.rfind(os.sep) + 1 :].strip()
+    f = f"{base}_{slot:05d}"
+    return canonicalizename(f)
 
 
-def kkscript_figureproperties( _config) :
-    fig_opts = _config.opts
-    if fig_opts is None :
-        fig_opts = dict()
-    o = lambda _key, _default : str(fig_opts.get( _key, _default))
-    return '''
+def readheader(filename: str, delim: str):
+    with open(filename, "r", encoding="utf-8") as f:
+        header_line = f.readline()
+    cols = header_line.split(delim)
+
+    def unit_offs(length, pos):
+        return length if pos == -1 else pos
+
+    return [c[: unit_offs(len(c), c.find("["))] for c in cols]
+
+
+def findplotheaders(header, columns_str):
+    if columns_str[0] == "+":
+        # all columns after a given one
+        base = columns_str[1:]
+        return header[header.index(base) + 1 :]
+
+    selected = []
+    columns = columns_str.split(",")
+    # example: columns="c1,c2,c3:c4,c5:"
+    for column_range in columns:
+        if ":" in column_range:
+            c1, c2 = column_range.split(":")
+            if c1 == "" and c2 == "":
+                selected += header[:]
+            elif c2 == "":
+                selected += header[header.index(c1) :]
+            else:
+                selected += header[header.index(c1) : header.index(c2) + 1]
+        else:
+            selected.append(header[header.index(column_range)])
+    kklog_debug("plot-headers=" + str(selected))
+    return selected
+
+
+def kkscript_preamble(config: KKGenConfiguration) -> str:
+    return f'engine: "{config.engine}"'
+
+
+def kkscript_datasources(config: KKGenConfiguration) -> str:
+    lines = []
+    for i, fname in enumerate(config.datafiles):
+        fid = datasourceid(fname, i)
+        lines.append(f'  {fid}:\n    path: "{fname}"')
+    return "datasources:\n" + "\n".join(lines)
+
+
+def kkscript_figureproperties(config: KKGenConfiguration) -> str:
+    fig_opts = config.opts or {}
+    o = lambda key, default: str(fig_opts.get(key, default))
+
+    return f"""
 figure:
-  title: %s
-  time: '%s'
+  title: {config.title}
+  time: '{config.range}'
   output: 'output.pdf'
   properties:
     #colorscheme: grayscale
     alignmentorder: 'rowsfirstdownward'
-    square: %s
-    columns: %s
-    rows: %s
-    height: %s
-    width: %s
+    square: {o('square', 'false')}
+    columns: {o('columns', 0)}
+    rows: {o('rows', 0)}
+    height: {o('height', 'null')}
+    width: {o('width', 'null')}
     legendfontsize: 10
-''' % ( _config.title, _config.range, o('square','false'), o('columns',0), o('rows',0), o('height','null'), o('width','null'))
-
-def kkscript_plots_separate( _config) :
-    graphs = list()
-    for l, fname in enumerate( _config.datafiles) :
-        fid = datasourceid( fname, l)
-        fheader = readheader( fname, _config.columnsdelim)
-        fcolumns = findplotheaders( fheader, _config.columns)
-        for column in fcolumns :
-            graphs.append( '- %s:\n      graphs:\n        - %s:\n            name: [ \"%s@%s\" ]\n            label: \"%s\"' \
-                % ( '%s_%s' % ( column, fid), column, column, fid, column.replace( '_', ' ').title()))
-
-    return 'plots:\n  %s' % ( '\n  '.join( graphs))
-
-def kkscript_plots_diff( _config) :
-    graphs = list()
-    fheader = readheader( _config.datafiles[0], _config.columnsdelim)
-    fcolumns = findplotheaders( fheader, _config.columns)
-    for column in fcolumns :
-        graph = '- %s:\n      graphs:\n' % ( column)
-        D = len( _config.datafiles)
-        for l, fname in enumerate( _config.datafiles) :
-            fid = datasourceid( fname, l)
-            lblname = _config.names[D-l-1]
-            graph += '        - %s:\n            name: [ \"%s@%s\" ]\n            label: \"%s [%s]\"\n' \
-                % ( '%s_%s' % ( column, fid), column, fid, column.replace( '_', ' ').title(), lblname)
-
-        graphs.append( graph)
-
-    return 'plots:\n  %s' % ( '\n  '.join( graphs))
-
-def kkgen_showcolumns( _config) :
-    for l, fname in enumerate( _config.datafiles) :
-        header = readheader( fname, _config.columnsdelim)
-        columns = findplotheaders( header, _config.columns)
-        print '%s:' % ( fname)
-        print '  '+'\n  '.join( [ '%3d  %s'%(i+1,n) for i, n in enumerate( columns)])
- 
-def kkscript_plots( _config) :
-    if _config.diff :
-        return kkscript_plots_diff( _config)
-    return kkscript_plots_separate( _config)
-
-def kkgen_generate( _config, _plot_fn) :
-    kklog_debug( 'files=%s' % ( str(_config.datafiles)))
-    kklog_debug( 'columns=%s' % ( _config.columns))
-
-    if _config.showcolumns :
-        kkgen_showcolumns( _config)
-    else :
-        plots = _plot_fn( _config)
-        print kkscript_preamble( _config)
-        print '\n'
-        print kkscript_datasources( _config)
-        print '\n'
-        print kkscript_figureproperties( _config)
-        print '\n'
-        print plots
+""".lstrip("\n")
 
 
-def main( _plot_fn=None, _fig_opts=dict()) :
-    try :
-        kkgen_config = kkgen_configuration()
-    except :
+def kkscript_plots_separate(config: KKGenConfiguration) -> str:
+    graphs = []
+    for i, fname in enumerate(config.datafiles):
+        fid = datasourceid(fname, i)
+        header = readheader(fname, config.columnsdelim)
+        columns = findplotheaders(header, config.columns)
+        for column in columns:
+            label = column.replace("_", " ").title()
+            graphs.append(
+                f"- {column}_{fid}:\n"
+                f"      graphs:\n"
+                f"        - {column}:\n"
+                f"            name: [ \"{column}@{fid}\" ]\n"
+                f"            label: \"{label}\""
+            )
+    return "plots:\n  " + "\n  ".join(graphs)
+
+
+def kkscript_plots_diff(config: KKGenConfiguration) -> str:
+    graphs = []
+    header = readheader(config.datafiles[0], config.columnsdelim)
+    columns = findplotheaders(header, config.columns)
+    D = len(config.datafiles)
+    for column in columns:
+        graph = f"- {column}:\n      graphs:\n"
+        for i, fname in enumerate(config.datafiles):
+            fid = datasourceid(fname, i)
+            lblname = config.names[D - i - 1]
+            label = column.replace("_", " ").title()
+            graph += (
+                f"        - {column}_{fid}:\n"
+                f"            name: [ \"{column}@{fid}\" ]\n"
+                f"            label: \"{label} [{lblname}]\"\n"
+            )
+        graphs.append(graph.rstrip("\n"))
+    return "plots:\n  " + "\n  ".join(graphs)
+
+
+def kkgen_showcolumns(config: KKGenConfiguration) -> None:
+    for fname in config.datafiles:
+        header = readheader(fname, config.columnsdelim)
+        columns = findplotheaders(header, config.columns)
+        print(f"{fname}:")
+        for i, name in enumerate(columns, start=1):
+            print(f"  {i:3d}  {name}")
+
+
+def kkscript_plots(config: KKGenConfiguration) -> str:
+    if config.diff:
+        return kkscript_plots_diff(config)
+    return kkscript_plots_separate(config)
+
+
+def kkgen_generate(config: KKGenConfiguration, plot_fn) -> None:
+    kklog_debug("files=%s" % str(config.datafiles))
+    kklog_debug("columns=%s" % config.columns)
+    if config.showcolumns:
+        kkgen_showcolumns(config)
+    else:
+        plots = plot_fn(config)
+        print(kkscript_preamble(config))
+        print()
+        print(kkscript_datasources(config))
+        print()
+        print(kkscript_figureproperties(config))
+        print()
+        print(plots)
+
+
+def main(plot_fn=None, fig_opts=None) -> int:
+    if fig_opts is None:
+        fig_opts = {}
+
+    try:
+        config = KKGenConfiguration()
+    except Exception:
+        # 65 is EX_DATAERR in sysexits.h; keep behavior if you rely on it
         return 65
 
-    kkgen_config.opts.update( _fig_opts)
-    if kkgen_config.showversion :
-        sys.stdout.write( '%s %s\n' % ( utils.programname(), str(__version__)))
-    else :
-        plot_fn = _plot_fn
-        if plot_fn is None :
-            plot_fn = kkscript_plots
-        kkgen_generate( kkgen_config, plot_fn)
+    config.opts.update(fig_opts)
+
+    if config.showversion:
+        sys.stdout.write(f"{utils.programname()} {__version__}\n")
+        return 0
+
+    if plot_fn is None:
+        plot_fn = kkscript_plots
+
+    kkgen_generate(config, plot_fn)
     return 0
 
-if __name__ == '__main__':
 
+if __name__ == "__main__":
     rc = main()
-    sys.exit( rc)
-
+    sys.exit(rc)
